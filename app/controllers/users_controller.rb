785b8a0d8764d46200db1d694637fdd8ab4f6ee5
shimo_yuki <c0116138b7@edu.teu.ac.jp>
2019-10-25 12:10:35 +0900

    窓関数!

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,11 @@ class UsersController < ApplicationController
     def index
         @users = User.all
         @user_arr = @users.to_json.html_safe
-        
+    end
+
+    def show
+        @user = User.find(params[:id])
+        @user_arr = @user.to_json.html_safe
     end
     def create
         user = User.new
